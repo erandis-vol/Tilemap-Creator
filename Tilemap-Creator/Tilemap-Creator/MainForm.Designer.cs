899599a548bc7dce697338ebba421dc3ac36606f
Lostelle <jack.bulan@gmail.com>
2016-08-07 21:53:06 -0400

    Basic Tileset UI

--- Tilemap-Creator/Tilemap-Creator/MainForm.Designer.cs
+++ Tilemap-Creator/Tilemap-Creator/MainForm.Designer.cs
@@ -28,23 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.tilesetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.createToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.tilemapToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.cTilesetWidth = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.tTilesetHeight = new TMC.NumericToolStripTextBox();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
             this.tTilemapWidth = new TMC.NumericToolStripTextBox();
             this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
             this.tTilemapHeight = new TMC.NumericToolStripTextBox();
+            this.bResizeTilemap = new System.Windows.Forms.ToolStripButton();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.pTileset = new TMC.InterpolatedPictureBox();
             this.pTilemap = new TMC.InterpolatedPictureBox();
-            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.openToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
-            this.saveToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
-            this.createToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.menuStrip1.SuspendLayout();
             this.toolStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
@@ -78,6 +86,32 @@
             this.tilesetToolStripMenuItem.Size = new System.Drawing.Size(64, 24);
             this.tilesetToolStripMenuItem.Text = "Tileset";
             // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(127, 26);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(127, 26);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(124, 6);
+            // 
+            // createToolStripMenuItem
+            // 
+            this.createToolStripMenuItem.Name = "createToolStripMenuItem";
+            this.createToolStripMenuItem.Size = new System.Drawing.Size(127, 26);
+            this.createToolStripMenuItem.Text = "Create";
+            this.createToolStripMenuItem.Click += new System.EventHandler(this.createToolStripMenuItem_Click);
+            // 
             // tilemapToolStripMenuItem
             // 
             this.tilemapToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
@@ -87,39 +121,91 @@
             this.tilemapToolStripMenuItem.Size = new System.Drawing.Size(75, 24);
             this.tilemapToolStripMenuItem.Text = "Tilemap";
             // 
+            // openToolStripMenuItem1
+            // 
+            this.openToolStripMenuItem1.Name = "openToolStripMenuItem1";
+            this.openToolStripMenuItem1.Size = new System.Drawing.Size(120, 26);
+            this.openToolStripMenuItem1.Text = "Open";
+            // 
+            // saveToolStripMenuItem1
+            // 
+            this.saveToolStripMenuItem1.Name = "saveToolStripMenuItem1";
+            this.saveToolStripMenuItem1.Size = new System.Drawing.Size(120, 26);
+            this.saveToolStripMenuItem1.Text = "Save";
+            // 
             // toolStrip1
             // 
             this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
             this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cTilesetWidth,
+            this.toolStripLabel2,
+            this.tTilesetHeight,
+            this.toolStripSeparator2,
             this.tTilemapWidth,
             this.toolStripLabel1,
-            this.tTilemapHeight});
+            this.tTilemapHeight,
+            this.bResizeTilemap});
             this.toolStrip1.Location = new System.Drawing.Point(0, 28);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(676, 27);
+            this.toolStrip1.Size = new System.Drawing.Size(676, 28);
             this.toolStrip1.TabIndex = 1;
             this.toolStrip1.Text = "toolStrip1";
+            // 
+            // cTilesetWidth
+            // 
+            this.cTilesetWidth.Name = "cTilesetWidth";
+            this.cTilesetWidth.Size = new System.Drawing.Size(121, 28);
+            this.cTilesetWidth.Text = "0";
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(16, 25);
+            this.toolStripLabel2.Text = "x";
+            // 
+            // tTilesetHeight
+            // 
+            this.tTilesetHeight.Name = "tTilesetHeight";
+            this.tTilesetHeight.ReadOnly = true;
+            this.tTilesetHeight.Size = new System.Drawing.Size(100, 28);
+            this.tTilesetHeight.Text = "0";
+            this.tTilesetHeight.Value = 0;
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 28);
             // 
             // tTilemapWidth
             // 
             this.tTilemapWidth.Name = "tTilemapWidth";
-            this.tTilemapWidth.Size = new System.Drawing.Size(100, 27);
+            this.tTilemapWidth.Size = new System.Drawing.Size(100, 28);
             this.tTilemapWidth.Text = "0";
             this.tTilemapWidth.Value = 0;
             // 
             // toolStripLabel1
             // 
             this.toolStripLabel1.Name = "toolStripLabel1";
-            this.toolStripLabel1.Size = new System.Drawing.Size(16, 24);
+            this.toolStripLabel1.Size = new System.Drawing.Size(16, 25);
             this.toolStripLabel1.Text = "x";
             // 
             // tTilemapHeight
             // 
             this.tTilemapHeight.Name = "tTilemapHeight";
-            this.tTilemapHeight.Size = new System.Drawing.Size(100, 27);
+            this.tTilemapHeight.Size = new System.Drawing.Size(100, 28);
             this.tTilemapHeight.Text = "0";
             this.tTilemapHeight.Value = 0;
+            // 
+            // bResizeTilemap
+            // 
+            this.bResizeTilemap.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bResizeTilemap.Image = ((System.Drawing.Image)(resources.GetObject("bResizeTilemap.Image")));
+            this.bResizeTilemap.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.bResizeTilemap.Name = "bResizeTilemap";
+            this.bResizeTilemap.Size = new System.Drawing.Size(24, 25);
+            this.bResizeTilemap.Text = "Resize Tilemap";
+            this.bResizeTilemap.Click += new System.EventHandler(this.bResizeTilemap_Click);
             // 
             // statusStrip1
             // 
@@ -134,7 +220,7 @@
             // 
             this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer1.Location = new System.Drawing.Point(0, 55);
+            this.splitContainer1.Location = new System.Drawing.Point(0, 56);
             this.splitContainer1.Name = "splitContainer1";
             // 
             // splitContainer1.Panel1
@@ -146,7 +232,7 @@
             // 
             this.splitContainer1.Panel2.AutoScroll = true;
             this.splitContainer1.Panel2.Controls.Add(this.pTilemap);
-            this.splitContainer1.Size = new System.Drawing.Size(676, 523);
+            this.splitContainer1.Size = new System.Drawing.Size(676, 522);
             this.splitContainer1.SplitterDistance = 326;
             this.splitContainer1.TabIndex = 3;
             // 
@@ -156,8 +242,10 @@
             this.pTileset.Location = new System.Drawing.Point(0, 0);
             this.pTileset.Name = "pTileset";
             this.pTileset.Size = new System.Drawing.Size(16, 16);
+            this.pTileset.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pTileset.TabIndex = 0;
             this.pTileset.TabStop = false;
+            this.pTileset.Paint += new System.Windows.Forms.PaintEventHandler(this.pTileset_Paint);
             // 
             // pTilemap
             // 
@@ -168,41 +256,11 @@
             this.pTilemap.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pTilemap.TabIndex = 0;
             this.pTilemap.TabStop = false;
+            this.pTilemap.Paint += new System.Windows.Forms.PaintEventHandler(this.pTilemap_Paint);
             // 
-            // openToolStripMenuItem
+            // openFileDialog1
             // 
-            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
-            this.openToolStripMenuItem.Text = "Open";
-            // 
-            // saveToolStripMenuItem
-            // 
-            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
-            this.saveToolStripMenuItem.Text = "Save";
-            // 
-            // openToolStripMenuItem1
-            // 
-            this.openToolStripMenuItem1.Name = "openToolStripMenuItem1";
-            this.openToolStripMenuItem1.Size = new System.Drawing.Size(181, 26);
-            this.openToolStripMenuItem1.Text = "Open";
-            // 
-            // saveToolStripMenuItem1
-            // 
-            this.saveToolStripMenuItem1.Name = "saveToolStripMenuItem1";
-            this.saveToolStripMenuItem1.Size = new System.Drawing.Size(181, 26);
-            this.saveToolStripMenuItem1.Text = "Save";
-            // 
-            // toolStripSeparator1
-            // 
-            this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(178, 6);
-            // 
-            // createToolStripMenuItem
-            // 
-            this.createToolStripMenuItem.Name = "createToolStripMenuItem";
-            this.createToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
-            this.createToolStripMenuItem.Text = "Create";
+            this.openFileDialog1.FileName = "openFileDialog1";
             // 
             // MainForm
             // 
@@ -252,6 +310,13 @@
         private System.Windows.Forms.ToolStripMenuItem createToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem1;
         private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripButton bResizeTilemap;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private NumericToolStripTextBox tTilesetHeight;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripComboBox cTilesetWidth;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
     }
 }
 
--- Tilemap-Creator/Tilemap-Creator/MainForm.cs
+++ Tilemap-Creator/Tilemap-Creator/MainForm.cs
@@ -18,6 +18,12 @@ namespace TMC
         Sprite tilesetImage;
         Bitmap tilemapImage;
 
+        int zoom = 2; // default zoom is 200%
+        bool ignore = false;
+
+        Color gridColor = Color.FromArgb(128, Color.White);
+        Color gridColorS = Color.FromArgb(128, Color.Yellow);
+
         public MainForm()
         {
             InitializeComponent();
@@ -30,9 +36,9 @@ namespace TMC
 
         private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
         {
-            tilesetImage?.Dispose();
             tileset?.Dispose();
-            
+
+            tilesetImage?.Dispose();
             tilemapImage?.Dispose();
         }
 
@@ -80,6 +86,215 @@ namespace TMC
         private void button2_Click(object sender, EventArgs e)
         {
             tilesetImage?.Save("test.bmp", SpriteFormat.BMP);
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // TODO: ask to save old Tileset/Tilemap
+
+            openFileDialog1.Title = "Open Tileset";
+            openFileDialog1.Filter = "Images|*.bmp;*.png;*.jpg";
+
+            if (openFileDialog1.ShowDialog() != DialogResult.OK) return;
+
+            // create new Tileset
+            using (var bmp = new Bitmap(openFileDialog1.FileName))
+            using (var sprite = new Sprite(bmp))
+            {
+                // sprite dimensions must be divisible by 8
+                if (sprite.Width % 8 != 0 || sprite.Height % 8 != 0)
+                {
+                    MessageBox.Show("Tileset source Sprite dimensions are not divisible by 8!", "Invalid Sprite", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                // Tileset from sprite
+                tileset?.Dispose();
+                tileset = new Tileset(sprite);
+            }
+
+            // fill sizes for Tileset
+            cTilesetWidth.Items.Clear();
+            foreach (var size in tileset.PerfectSizes)
+                cTilesetWidth.Items.Add(size.Width.ToString());
+
+            // pick middle size
+            cTilesetWidth.SelectedIndex = cTilesetWidth.Items.Count / 2;
+
+            // finish
+            UpdateTileset();
+
+            // create new blank Tilemap
+            tilemap = new Tilemap(30, 20);
+            UpdateTilemap();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (tileset == null || tilesetImage == null) return;
+
+            saveFileDialog1.Title = "Save Tileset";
+            saveFileDialog1.Filter = "Bitmaps|*.bmp";
+
+            if (saveFileDialog1.ShowDialog() != DialogResult.OK) return;
+
+            // save Tileset as .bmp (forced for now)
+            tilesetImage.Save(saveFileDialog1.FileName, SpriteFormat.BMP);
+        }
+
+        private void createToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // TODO: ask to save Tilemap/Tileset
+
+            openFileDialog1.Title = "Create Tileset";
+            openFileDialog1.Filter = "Images|*.bmp;*.png;*.jpg";
+
+            if (openFileDialog1.ShowDialog() != DialogResult.OK) return;
+
+            using (var bmp = new Bitmap(openFileDialog1.FileName))
+            using (var sprite = new Sprite(bmp))
+            {
+                // Sprite dimensions must be divisible by 8
+                if (sprite.Width % 8 != 0 || sprite.Height % 8 != 0)
+                {
+                    MessageBox.Show("Tileset source image dimensions are not divisible by 8!", "Invalid Image", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                // create Tilemap/Tileset
+                tileset?.Dispose();
+                Tileset.Create(sprite, false, out tileset, out tilemap);
+            }
+
+            // fill sizes for Tileset
+            cTilesetWidth.Items.Clear();
+            foreach (var size in tileset.PerfectSizes)
+                cTilesetWidth.Items.Add(size.Width.ToString());
+
+            // pick middle size
+            cTilesetWidth.SelectedIndex = cTilesetWidth.Items.Count / 2;
+
+            // finish
+            UpdateTileset();
+            UpdateTilemap();
+        }
+
+        // updates Tileset size and image
+        void UpdateTileset()
+        {
+            if (tileset == null) return;
+            ignore = true;
+
+            Console.WriteLine("updating Tileset");
+
+            // get Tileset size
+            int width;
+            if (!int.TryParse(cTilesetWidth.Text, out width))
+                width = 1;
+
+            int height = (tileset.Size / width) + (tileset.Size % width > 0 ? 1 : 0);
+
+            // update height text
+            tTilesetHeight.Value = height;
+
+            // update Tileset image
+            tilesetImage?.Dispose();
+            tilesetImage = tileset.Smoosh(width);
+
+            pTileset.Size = new Size(tilesetImage.Width * zoom, tilesetImage.Height * zoom);
+            pTileset.Image = tilesetImage;
+            ignore = false;
+        }
+
+        // updates Tilemap image (forced redraw)
+        void UpdateTilemap()
+        {
+            if (tilemap == null || tileset == null) return;
+            ignore = true;
+
+            Console.WriteLine("updating Tilemap");
+
+            // set size info
+            tTilemapWidth.Value = tilemap.Width;
+            tTilemapHeight.Value = tilemap.Height;
+
+            // recreate Tilemap image
+            tilemapImage?.Dispose();
+            tilemapImage = new Bitmap(tilemap.Width * Tileset.TileSize, tilemap.Height * Tileset.TileSize);
+
+            // render Tilemap fully
+            using (var g = Graphics.FromImage(tilemapImage))
+            {
+                for (int y = 0; y < tilemap.Height; y++)
+                {
+                    for (int x = 0; x < tilemap.Width; x++)
+                    {
+                        var tileIndex = tilemap[x, y].TilesetIndex;
+                        var tile = tileset[tileIndex];
+                        g.DrawImage(tile, x * Tileset.TileSize, y * Tileset.TileSize);
+                    }
+                }
+            }
+
+            // TODO: render Palettemap fully
+
+            // finished
+            pTilemap.Size = new Size(tilemapImage.Width * zoom, tilemapImage.Height * zoom);
+            pTilemap.Image = tilemapImage;
+            ignore = false;
+        }
+
+        private void pTileset_Paint(object sender, PaintEventArgs e)
+        {
+            if (tileset == null) return;
+
+            // draw grid
+            using (var pen = new Pen(new SolidBrush(gridColor), 1f))
+            {
+                pen.DashPattern = new[] { 2f, 2f };
+
+                int f = zoom * Tileset.TileSize;
+
+                for (int x = 1; x < pTileset.Width / f; x++)
+                {
+                    e.Graphics.DrawLine(pen, x * f, 0, x * f, pTileset.Height);
+                }
+
+                for (int y = 1; y < pTileset.Height / f; y++)
+                {
+                    e.Graphics.DrawLine(pen, 0, y * f, pTileset.Width, y * f);
+                }
+            }
+        }
+
+        private void pTilemap_Paint(object sender, PaintEventArgs e)
+        {
+            if (tileset == null || tilemap == null) return;
+
+            // draw grid
+            using (var pen = new Pen(new SolidBrush(gridColor), 1f))
+            using (var penS = new Pen(new SolidBrush(gridColorS), 1f))
+            {
+                pen.DashPattern = new[] { 2f, 2f };
+                penS.DashPattern = new[] { 2f, 2f };
+
+                int f = zoom * Tileset.TileSize;
+
+                for (int x = 1; x < pTilemap.Width / f; x++)
+                {
+                    e.Graphics.DrawLine(x % 30 == 0 ? penS : pen, x * f, 0, x * f, pTilemap.Height);
+                }
+
+                for (int y = 1; y < pTilemap.Height / f; y++)
+                {
+                    e.Graphics.DrawLine(y % 20 == 0 ? penS : pen, 0, y * f, pTilemap.Width, y * f);
+                }
+            }
+        }
+
+        private void bResizeTilemap_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
